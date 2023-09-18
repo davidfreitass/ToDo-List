3c0344c7cf24c5a212021047aae04154afe7ba0b
davidfreitass <davidcnt@outlook.com>
2023-09-18 20:10:59 -0300

    Adicionando Schema e permitindo realizar um GET

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, jsonify
 # Importando o Flask
 from flask_sqlalchemy import SQLAlchemy
 # Importando o SQLAlchemy para usar juntamente ao Flask
+from marshmallow import Schema, fields
 
 app = Flask(__name__)  # Aplicação principal do Flask
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database3.db"
@@ -14,8 +15,13 @@ class ItemTodo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200))
 
-    def __repr__(self):
-        return f'ItemTodo {self.id}'
+
+class ItemTodoSchema(Schema):
+    id = fields.Int(dump_only=True)
+    content = fields.Str()
+
+
+itemstodo_schema = ItemTodoSchema(many=True)
 
 
 # Decorator serve para uma função atribuir uma nova funcionalidade para outra função
@@ -24,18 +30,32 @@ class ItemTodo(db.Model):
 def index():
     # Buscando todos os objetos ItemTodo existentes no banco de dados
     items = ItemTodo.query.all()
-    return items
+    result = itemstodo_schema.dump(items)
+    return {"items": result}
 
 
 @app.route("/post/", methods=["POST"])
 def post():
     # Criando um novo objeto ItemTodo e adicionando no banco de dados
-    content = request.get_json()
-    if content is None:
-        new_item = ItemTodo(content=content)
+    dados_request = request.get_json()  # Recebendo todo um objeto enviado no POST
+    print(dados_request)
+    if dados_request is not None:
+        # Tratando esse objeto e captando somente o necessário
+        new_item = ItemTodo(content=dados_request['content'])
         db.session.add(new_item)
         db.session.commit()
-    return redirect("/")
+        return jsonify(
+            message="Item adicionado com sucesso",
+            category="success",
+            data=dados_request,
+            status=200
+        )
+    else:
+        return jsonify(
+            message="No send content data",
+            category="error",
+            status=404
+        )
 
 
 if __name__ == "__main__":
