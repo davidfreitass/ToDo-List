af656efde33eeec688b4ecb56504bcdf4037b890
davidfreitass <davidcnt@outlook.com>
2023-09-25 16:28:37 -0300

    Possibilitando atualizar um item através do método PUT

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 # Importando o Flask
 from flask_sqlalchemy import SQLAlchemy
 # Importando o SQLAlchemy para usar juntamente ao Flask
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError
 # Schema é uma biblioteca para validação de estruturas de dados em Python, fields é uma classe que recebe vários tipos de dados
 
 app = Flask(__name__)  # Aplicação principal do Flask
@@ -31,6 +31,7 @@ itemtodo_schema = ItemTodoSchema()
 # Método route faz com que possamos definir uma rota para uma página
 @app.route("/", methods=["GET"])
 @app.route("/todo", methods=["GET"])
+@app.route("/todo/", methods=["GET"])
 def index():
     # Buscando todos os objetos ItemTodo existentes no banco de dados
     items = ItemTodo.query.all()
@@ -46,27 +47,24 @@ def get_id(id):
     return {"item": result}
 
 
-# @app.route("/todo/<int:id>", methods=["PUT"])
-# def put(id):
-#     dados_request = request.get_json()
-#     if dados_request is not None:
-#         # Tratando esse objeto e captando somente o necessário
-#         item_updated = ItemTodo(content=dados_request['content'])
-#         item_id = ItemTodo.query.get(id)
-#         result = itemtodo_schema.dump(item_id)
+@app.route("/todo/<int:id>", methods=["PUT"])
+def update_todo(id):
+    dados_request = request.get_json()
+    if not dados_request:
+        return {"message": "No input data provided"}, 400
+    try:
+        dados = itemtodo_schema.load(dados_request)
+    except ValidationError as err:
+        return err.messages, 422
 
-#         return jsonify(
-#             message="Item atualizado com sucesso",
-#             category="success",
-#             data=dados_request,
-#             status=200
-#         )
-#     else:
-#         return jsonify(
-#             message="No send content data",
-#             category="error",
-#             status=404
-#         )
+    item_updated = ItemTodo.query.get(id)
+    item_updated.content = dados["content"]
+
+    db.session.add(item_updated)
+    db.session.commit()
+    result = itemtodo_schema.dump(ItemTodo.query.get(item_updated.id))
+    return {"message": "Tarefa atualizada.", "tarefa": result}
+
 
 @app.route("/post", methods=["POST"])
 def post():
